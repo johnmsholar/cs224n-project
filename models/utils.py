9d0e28df4487edafa4dd22cdcdfcb575c89af76c
Saachi Jain <saachi@stanford.edu>
2017-02-28 19:39:57 -0800

    Read from glove vectors and save/load glove binary

--- models/utils.py
+++ models/utils.py
@@ -2,6 +2,8 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 import csv
 
+GLOVE_SIZE = 300
+
 def construct_data_set():
     # File Headers
     body_id_header = 'Body ID'
@@ -19,6 +21,50 @@ def construct_data_set():
 
     with open('../fnc_1/train_stances.csv') as stances_file:
         stances_reader = csv.reader(stances_file, delimiter = ',')
+
+def read_glove_set():
+    default = np.zeros(GLOVE_SIZE)
+    glove_vectors = defaultdict(default)
+    id_to_glove_body = {}
+    with open("../glove.6B/glove.6B.300d.txt") as glove_files:
+        glove_reader = csv.DictReader(glove_files, delimiter = ' ')
+        for row in glove_reader:
+            word = row[0]
+            vec = np.array(row[1:])
+            glove_vector[word] = vec
+    return glove_vectors
+
+def save_glove_sums_matrix(
+    glove_vectors, body_fname="glove_body_matrix.txt",
+    headline_fname="glove_headline_matrix.txt"):
+    # read body
+    glove_body_matrix = np.zeros((len(id_to_body), GLOVE_SIZE))
+    for (body_id, text) in id_to_body.items():
+        body_sum = np.zeros((GLOVE_SIZE))
+        for word in text:
+            body_sum += glove_vectors[word]
+        glove_body_matrix[body_id] = body_sum
+    # read headline
+    glove_headline_matrix = np.zeros((len(id_to_headline),GLOVE_SIZE))
+    for (headline_id, text) in id_to_headline.items():
+        headline_sum = np.zeros((GLOVE_SIZE))
+        for word in text:
+            headline_sum += glove_vectors[word]
+        glove_headline_matrix[body_id] = headline_sum
+    # saves as np binaries glove_body | glove_headline
+    np.save(body_fname, glove_body_matrix)
+    np.save(headline_fname, glove_headline_matrix)
+
+def read_glove_sums(
+    body_fname="glove_body_matrix.txt",
+    headline_fname="glove_headline_matrix.txt"):
+
+    glove_body_matrix = np.load(body_fname)
+    glove_headline_matrix = np.load(headline_fname)
+    return glove_body_matrix, glove_headline_matrix
+
+def compute_word_embeddings():
+    headline
 
 
 
