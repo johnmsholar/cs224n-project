15e2ce369cf58dcb3700f4a13f8187160510612e
John Sholar <johnmsholar@gmail.com>
2017-02-28 21:56:48 -0800

    Modified net.py to preprocess embedding vectors.

--- models/net.py
+++ models/net.py
@@ -6,13 +6,32 @@ from sklearn.model_selection import train_test_split
 # Define Neural Net Parameters
 # TODO: Change INPUT_DIM and NUM_CLASSES appropriately (currently set for training example)
 
-INPUT_DIM = 2
-NUM_CLASSES = 2
+INITIAL_EMBEDDING_DIM = 300
+REDUCED_EMBEDDING_DIM = 100
+INPUT_DIM = 200
+NUM_CLASSES = 4
 LEARNING_RATE = .5
 NUM_ITERATIONS = 1000
 
 weight_names = ['W1', 'W2', 'W3']
 bias_names = ['b1', 'b2', 'b3']
+
+# Declare tf.placeholders for input data (X) and input labels (Y)
+input_labels = tf.placeholder(tf.float32, (None, NUM_CLASSES))
+article_input_matrix = tf.placeholder(tf.float32, (None, INITIAL_EMBEDDING_DIM))
+headline_input_matrix = tf.placeholder(tf.float32, (None, INITIAL_EMBEDDING_DIM))
+
+# Declare initial tf.Variable matrices to map from initial embeddings to reduced dimension embeddings
+initial_article_weights = tf.Variable(tf.random_normal(shape=(INITIAL_EMBEDDING_DIM, REDUCED_EMBEDDING_DIM)))
+initial_article_bias = tf.Variable(tf.zeros(shape=(1, REDUCED_EMBEDDING_DIM)))
+initial_headline_weights = tf.Variable(tf.random_normal(shape=(INITIAL_EMBEDDING_DIM, REDUCED_EMBEDDING_DIM)))
+initial_headline_bias = tf.Variable(tf.zeros(shape=(1, REDUCED_EMBEDDING_DIM)))
+
+# Declare initial layer as a combination of article and headline initial layers
+initial_article_layer = tf.nn.tanh(tf.matmul(article_input_matrix, initial_article_weights) + initial_article_bias)
+initial_headline_layer = tf.nn.tanh(tf.matmul(headline_input_matrix, initial_headline_weights) + initial_headline_bias)
+initial_layer = tf.concat(values=[initial_article_layer, initial_headline_layer], concat_dim=1)
+
 
 # Declare tf.Variable weight matrices and bias vectors for 3 tanh layers
 weights = [
@@ -34,12 +53,8 @@ final_biases = tf.Variable(
   name='FinalBiases'
 )
 
-# Declare tf.placeholders for input data (X) and input labels (Y)
-input_matrix = tf.placeholder(tf.float32, (None, INPUT_DIM))
-input_labels = tf.placeholder(tf.float32, (None, NUM_CLASSES))
-
 # Declare intermediate variables as products of base variables for tanh layers and final layer
-layer_1 = tf.nn.tanh(tf.matmul(input_matrix, weights[0]) + biases[0])
+layer_1 = tf.nn.tanh(tf.matmul(initial_layer, weights[0]) + biases[0])
 layer_2 = tf.nn.tanh(tf.matmul(layer_1, weights[1]) + biases[1])
 layer_3 = tf.nn.tanh(tf.matmul(layer_2, weights[2]) + biases[2])
 final_layer = tf.matmul(layer_3, final_weights) + final_biases
@@ -50,6 +65,7 @@ loss = tf.nn.softmax_cross_entropy_with_logits(labels=input_labels, logits=final
 # Define train_step to be called repeatedly to optimize classification
 train_step = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(loss)
 
+"""
 # Generate Sample Dataset in 2-dimensional space
 # Class 1 is a Gaussian Centered Around (0, -5)
 # Class 2 is a Gaussian Centered Around (0, 5)
@@ -75,6 +91,7 @@ all_labels = np.concatenate([labels_1, labels_2], axis=0)
 train_data, test_data, train_labels, test_labels = train_test_split(
   all_data, all_labels, test_size=0.2, random_state=42
 )
+"""
 
 # Run TF Session
 # 1000 Train Iterations on Sample Data
@@ -82,17 +99,15 @@ train_data, test_data, train_labels, test_labels = train_test_split(
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 for iteration in range(NUM_ITERATIONS):
-  batch_vectors = train_data
-  batch_labels = train_labels
-  sess.run(train_step,
-    feed_dict = { input_matrix: batch_vectors, input_labels: batch_labels }
-  )
+  pass
 
 # Evaluate performance on test set
-print sess.run(loss, feed_dict={ input_matrix: test_data, input_labels: test_labels})
+# print sess.run(loss, feed_dict={ input_matrix: test_data, input_labels: test_labels})
 
 # Plot data
+"""
 plt.plot(x1, y1, 'x')
 plt.plot(x2, y2, 'x')
 plt.axis('equal')
 plt.savefig('plot.png', bbox_inches='tight')
+"""
