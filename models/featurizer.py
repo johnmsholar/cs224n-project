e1095249ceda842984a5becd970fb41780dc806c
schopra8 <schopra8@stanford.edu>
2017-03-02 00:40:26 -0800

    Fixed featurizer imports

--- models/featurizer.py
+++ models/featurizer.py
@@ -2,6 +2,7 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 from collections import defaultdict
 
+from models import LABEL_MAPPING
 from enum import Enum
 import csv
 import re
