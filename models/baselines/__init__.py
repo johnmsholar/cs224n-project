17c3eff01a130bf0d151a0ce9cc97115ffd44fae
John Sholar <johnmsholar@gmail.com>
2017-03-03 22:32:19 -0800

    Properly formatting __init__.py in models/baselines

---
+++ models/baselines/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+"""
+CS 224N 2016-2017
+__init__.py: Defines baselines module.
+Sahil Chopra <schopra8@cs.stanford.edu>
+Saachi Jain <saachi@cs.stanford.edu>
+John Sholar <jmsholar@cs.stanford.edu>
+"""
