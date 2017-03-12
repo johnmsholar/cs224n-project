97e0ffc68e41d45213409cfba496d2a76d8c2361
John Sholar <johnmsholar@gmail.com>
2017-03-11 18:50:27 -0800

    Fixed small bug in attention layer. Configured conditional_lstm to work with updated advanced_model API. Put conditional_lstm in debug mode.

--- models/advanced/conditional_lstm.py
+++ models/advanced/conditional_lstm.py
@@ -19,7 +19,7 @@ sys.path.insert(0, '../')
 
 from advanced_model import Advanced_Model, create_data_sets_for_model
 from fnc1_utils.score import report_score
-from fnc1_utils.featurizer import create_inputs_by_glove
+from fnc1_utils.featurizer import create_inputs_by_glove, create_embeddings
 from util import create_tensorflow_saver
 
 class Config:
@@ -28,8 +28,8 @@ class Config:
     information parameters. Model objects are passed a Config() object at
     instantiation. Use self.config.? instead of Config.?
     """
-    num_classes = 4 # Number of classses for classification task.
-    embed_size = 300 # Size of Glove Vectors
+    num_classes = 3 # Number of classses for classification task.
+    embed_size = 2 # Size of Glove Vectors
 
     # Hyper Parameters
     hidden_size = 300 # Hidden State Size
@@ -52,7 +52,7 @@ class Conditonal_Encoding_LSTM_Model(Advanced_Model):
         preds_fn = 'conditional_encoding_lstm_predicted.pkl'
         return [best_weights_fn, curr_weights_fn, preds_fn]
 
-    def add_prediction_op(self): 
+    def add_prediction_op(self, debug):
         """Runs RNN on the input. 
         """
         # Lookup Glove Embeddings for the headline words (e.g. one at each time step)
@@ -70,12 +70,12 @@ class Conditonal_Encoding_LSTM_Model(Advanced_Model):
         # run first headline LSTM
         with tf.variable_scope("headline_cell"):
             cell_headline = tf.contrib.rnn.LSTMBlockCell(num_units=self.config.hidden_size)
-            _, headline_state = tf.nn.dynamic_rnn(cell_headline, headline_x, dtype=tf.float32, sequence_length = self.h_seq_lengths_placeholder)
+            headline_outputs, headline_state = tf.nn.dynamic_rnn(cell_headline, headline_x, dtype=tf.float32, sequence_length = self.h_seq_lengths_placeholder)
 
         # run second LSTM that accept state from first LSTM
         with tf.variable_scope("body_cell"):
             cell_body = tf.contrib.rnn.LSTMBlockCell(num_units = self.config.hidden_size)
-            outputs, _ = tf.nn.dynamic_rnn(cell_body, body_x, initial_state=headline_state, dtype=tf.float32, sequence_length = self.a_seq_lengths_placeholder)
+            outputs, article_state = tf.nn.dynamic_rnn(cell_body, body_x, initial_state=headline_state, dtype=tf.float32, sequence_length = self.a_seq_lengths_placeholder)
 
         output = outputs[:,-1,:]
         assert output.get_shape().as_list() == [None, self.config.hidden_size], "predictions are not of the right shape. Expected {}, got {}".format([None, self.config.hidden_size], output.get_shape().as_list())
@@ -84,7 +84,21 @@ class Conditonal_Encoding_LSTM_Model(Advanced_Model):
         output_dropout = tf.nn.dropout(output, dropout_rate)
         preds = tf.matmul(output_dropout, U) + b
         assert preds.get_shape().as_list() == [None, self.config.num_classes], "predictions are not of the right shape. Expected {}, got {}".format([None, self.config.num_classes], preds.get_shape().as_list())
-        return preds
+
+        # Debugging Ops
+        if debug:
+            headline_x = tf.Print(headline_x, [headline_x], 'headline_x',summarize=20)
+            body_x = tf.Print(body_x, [body_x], 'body_x', summarize=24)
+            h_seq_lengths = tf.Print(self.h_seq_lengths_placeholder, [self.h_seq_lengths_placeholder],'h_seq_lengths', summarize=3)
+            a_seq_lengths = tf.Print(self.a_seq_lengths_placeholder, [self.a_seq_lengths_placeholder],'a_seq_lengths', summarize=3)
+            headline_outputs = tf.Print(headline_outputs, [headline_outputs], 'headline_outputs', summarize=20)
+            headline_state = tf.Print(headline_state, [headline_state], 'headline_state', summarize=20)
+            article_state = tf.Print(article_state, [article_state], 'article_state', summarize=20)
+            debug_ops = [headline_x, body_x, h_seq_lengths, a_seq_lengths, headline_outputs, headline_state, article_state]
+        else:
+            debug_ops = None
+
+        return preds, debug_ops
 
 def main(debug=True):
     # Parse Arguments
@@ -99,15 +113,24 @@ def main(debug=True):
         config.n_epochs = args.epoch
 
     # Load Data
-    X_train_input, X_dev_input, X_test_input, y_train_input, y_dev_input, y_test_input, glove_matrix, max_lengths= create_inputs_by_glove(concatenate=False)
-    train_examples, dev_set, test_set = create_data_sets_for_model(
-        X_train_input,
-        X_dev_input,
-        X_test_input,
-        y_train_input,
-        y_dev_input,
-        y_test_input
+    # X_train_input, X_dev_input, X_test_input, y_train_input, y_dev_input, y_test_input, glove_matrix, max_lengths= create_inputs_by_glove(concatenate=False)
+
+    X, y, glove_matrix, max_input_lengths, word_to_glove_index = create_embeddings(
+        training_size=1.0,
+        random_split=True,
+        truncate_headlines=False,
+        truncate_articles=True,
+        classification_problem=3,
+        max_headline_length=500,
+        max_article_length=500,
+        glove_set=None,
+        debug=debug
     )
+
+    train_examples, dev_set, test_set = create_data_sets_for_model(X, y)
+    print "Distribution of Train {}".format(np.sum(train_examples[4], axis=0))
+    print "Distribtion of Dev {}".format(np.sum(dev_set[4], axis=0))
+    print "Distribution of Test{}".format(np.sum(test_set[4], axis=0))
 
     with tf.Graph().as_default():
         print 80 * "="
@@ -116,8 +139,9 @@ def main(debug=True):
 
         # Create and configure model
         print "Building model...",
-        model = Conditonal_Encoding_LSTM_Model(config, report_score, max_lengths, glove_matrix)
         start = time.time()
+        model = Conditonal_Encoding_LSTM_Model(config, report_score, max_input_lengths, glove_matrix, debug=True)
+        model.print_params()
         print "took {:.2f} seconds\n".format(time.time() - start)
 
         # Initialize variables
@@ -152,4 +176,4 @@ def main(debug=True):
                 print "- test Score: {:.2f}".format(test_score)
 
 if __name__ == '__main__':
-    main(False)
+    main(True)
