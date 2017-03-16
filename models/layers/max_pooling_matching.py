79b2385709c2c1e377ca963d1bc3703f01b66f6c
Saachi Jain <saachi@stanford.edu>
2017-03-16 12:54:11 -0700

    max pooling is done

--- models/layers/max_pooling_matching.py
+++ models/layers/max_pooling_matching.py
@@ -1,4 +1,4 @@
-from attention_base_class import Attention_Base_Class, numpy_reference_compute_score
+from attention_base_class import Attention_Base_Class, numpy_reference_compute_score, numpy_check_equality, numpy_generate_A_B_w
 import sys
 import numpy as np
 
@@ -22,7 +22,7 @@ class Max_Pooling_Attention_Layer(Attention_Base_Class):
         # a_perm is now [A_time_steps, batch, hidden_size]
         a_perm = tf.transpose(a, perm=[1, 0, 2])
         a_time_steps = a_perm.get_shape()[0]
-
+        b_perm = tf.transpose(b, perm=[1, 0, 2])
         # h_n is [1 x batch x hidden_size]
         h_n =  tf.transpose(tf.expand_dims(b[:, -1, :], axis=0), perm=[1, 0, 2])
 
@@ -30,7 +30,7 @@ class Max_Pooling_Attention_Layer(Attention_Base_Class):
         cond = lambda i, result: tf.less(i, a_time_steps)
         def body(i, result):
             h_i = tf.expand_dims(a_perm[i, :, :], axis=0) # 1 x batch x hidden_size
-            m_i = self.compute_max_elem_score(h_i, b, W) # 1 x batch x perspectives
+            m_i = self.compute_max_elem_score(h_i, b_perm, W) # 1 x batch x perspectives
             result = tf.concat([result, m_i], axis=0) # building batch x time_steps x persepctive
             return [i+1, result]
 
@@ -38,6 +38,7 @@ class Max_Pooling_Attention_Layer(Attention_Base_Class):
         shape_invariants = [idx.get_shape(), tf.TensorShape([None, None, self.num_perspectives])]
         result = tf.zeros(shape=[1, batch_size, self.num_perspectives])
         result = tf.while_loop(cond, body, [idx, result], shape_invariants=shape_invariants) # A_time_steps x batch x perspectives
+        sliced_result = result[1][1:, :, :]
         return tf.transpose(result[1][1:, :, :], perm=[1, 0, 2]) # batch x time_steps x perspective
 
     def compute_max_elem_score(self, h_i, B, W, ):
@@ -48,7 +49,7 @@ class Max_Pooling_Attention_Layer(Attention_Base_Class):
         # Returns:
         #   1 x batch x perspectives which is max elem score over the scores on B
         b_time_steps = B.get_shape()[0]
-        cond = lambda j, result: tf.less(i, b_time_steps)
+        cond = lambda j, result: tf.less(j, b_time_steps)
         def body(j, result):
             h_j = tf.expand_dims(B[j, :, :], axis=0) # 1 x batch x hidden_size
             m_i_j = tf.expand_dims(self.compute_score(h_i, h_j, W), axis=0) # 1 x batch x perspectives
@@ -60,19 +61,25 @@ class Max_Pooling_Attention_Layer(Attention_Base_Class):
         result = tf.zeros(shape=[1, batch_size, self.num_perspectives])
         result = tf.while_loop(cond, body, [jdx, result], shape_invariants=shape_invariants) # B_time_steps x batch x perspectives
         result = result[1][1:, :, :] # B_time_steps x batch x perspective
-        result = tf.reduce_max(result, axis=0) # take elementwise maxx
+        result = tf.expand_dims(tf.reduce_max(result, axis=0), axis=0) # take elementwise max
         return result # 1 x batch x perspective
 
-def numpy_reference_fma(A, B, W, batch_size, A_time_steps, hidden_size, num_perspectives):
+def numpy_reference_mpa(A, B, W, batch_size, A_time_steps, hidden_size, num_perspectives):
+    # A [batch_size, A_time_steps, hidden_size]
+    # B [batch_size, B_time_steps, hidden_size]
+    # W [hidden_size, num_perspectives]
     result = np.zeros([A_time_steps, batch_size, num_perspectives])
     for i in range(0, A_time_steps):
-        a_slice = np.expand_dims(A[:, 1, :], axis=1)
-        b_slice = np.expand_dims(B[:, -1, :], axis=1)
+        a_slice = np.expand_dims(A[:, i, :], axis=1)
         h_i = np.transpose(a_slice, (1, 0, 2))
-        h_n = np.transpose(b_slice, (1, 0, 2))
-        result[i] = numpy_reference_compute_score(h_i, h_n, W, batch_size, hidden_size, num_perspectives)
+        i_result = np.zeros([B_time_steps, batch_size, num_perspectives])
+        for j in range(0, B_time_steps):
+            b_slice = np.expand_dims(B[:, j, :], axis=1)
+            h_j = np.transpose(b_slice, (1, 0, 2))
+            i_result[j] = numpy_reference_compute_score(h_i, h_j, W, batch_size, hidden_size, num_perspectives)
+        result[i] = np.amax(i_result, axis=0)
     return np.transpose(result, (1, 0, 2))
-
+    return result
 
 if __name__ == "__main__":
     with tf.Session() as session:
@@ -82,26 +89,9 @@ if __name__ == "__main__":
         A_time_steps = 5
         B_time_steps = 6
 
-        A = np.zeros([batch_size, A_time_steps, hidden_size])
-        B = np.zeros([batch_size, B_time_steps, hidden_size])
-        W = np.zeros([hidden_size, num_perspectives])
-        counter = 0
-        for i in range(0, batch_size):
-            for j in range(0, A_time_steps):
-                for k in range(0, hidden_size):
-                    A[i,j,k] = counter
-                    counter += 1
-        for i in range(0, batch_size):
-            for j in range(0, B_time_steps):
-                for k in range(0, hidden_size):
-                    B[i,j,k] = counter
-                    counter += 1
-        for i in range(0, hidden_size):
-            for j in range(0, num_perspectives):
-                W[i,j] = counter
-                counter += 1
-        fma = Max_Pooling_Attention_Layer(num_perspectives)
-        score_fn = fma.compute_attention(tf.constant(A, dtype=tf.float32), tf.constant(B, dtype=tf.float32), tf.constant(W, dtype=tf.float32))
+        A, B, W  = numpy_generate_A_B_w(batch_size, hidden_size, A_time_steps, B_time_steps, num_perspectives)
+        mpa = Max_Pooling_Attention_Layer(num_perspectives)
+        score_fn = mpa.compute_attention(tf.constant(A, dtype=tf.float32), tf.constant(B, dtype=tf.float32), tf.constant(W, dtype=tf.float32))
         score = session.run(score_fn)        
-        reference = numpy_reference_fma(A, B, W, batch_size, A_time_steps, hidden_size, num_perspectives)
-        assert score.all() == reference.all()
+        reference = numpy_reference_mpa(A, B, W, batch_size, A_time_steps, hidden_size, num_perspectives)
+        assert numpy_check_equality(score, reference, 1E-6)
