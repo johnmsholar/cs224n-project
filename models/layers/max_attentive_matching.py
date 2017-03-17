1dc3fbf704935d952ac62885f8ee79c41429573b
Saachi Jain <saachi@stanford.edu>
2017-03-17 00:39:20 -0700

    done with max matching

--- models/layers/max_attentive_matching.py
+++ models/layers/max_attentive_matching.py
@@ -1,9 +1,10 @@
-from attention_base_class import Attention_Base_Class, numpy_reference_compute_score, numpy_generate_A_B_w
-
+from attention_base_class import Attention_Base_Class, numpy_reference_compute_score, numpy_generate_A_B_w, numpy_check_equality
+from attentive_matching_layer import numpy_compute_alpha_embeddings
 import sys
 sys.path.insert(0, '../')
 import tensorflow as tf
 from util import multiply_3d_by_2d, cosine_similarity
+import numpy as np
 
 class Max_Attentive_Matching_Layer(Attention_Base_Class):
     """
@@ -11,54 +12,31 @@ class Max_Attentive_Matching_Layer(Attention_Base_Class):
     def compute_alpha_embeddings(self, h_i, H_q):
         """
         Args:
-            h_i: tensor [1, hidden_size, batch_size]
-            H_q: tensor [B_time_steps, hidden_size, batch_size]
+            h_i: tensor [hidden_size, batch_size]
+            H_q: tensor [B_time_steps, hidden_size, batch]
         Output:
             alpha: tensor[batch_size, B_time_steps]
             We compute alpha_(i,j) = cosine(h_i, h_j_q) between
             a slice at a single timestep i in H_p (h_i) and all the timesteps
             in H_q.
         """
-        print H_q.get_shape()
-        print h_i.get_shape()
-        batch_size = h_i.get_shape().as_list()[2]
-        hidden_size = h_i.get_shape().as_list()[1]
+        batch_size = h_i.get_shape().as_list()[1]
+        hidden_size = h_i.get_shape().as_list()[0]
         b_time_steps = H_q.get_shape().as_list()[0]
-        result_init = tf.zeros([batch_size, 1]) # batch_size x 1
 
-        # Cosine function requires dimensions hidden_size x batch
-        h_i_collapse = tf.reshape(h_i, shape=[hidden_size, batch_size])    
-
-        idx = tf.constant(0) # Current time step index
-        cond = lambda j, result: j < b_time_steps
         def body(j, result):
-            h_j = H_q[j, :, :] # 1 x hidden_size x batch
-            h_j_collapse = tf.reshape(h_j, shape=[hidden_size, batch_size])
-            alpha = cosine_similarity(h_i_collapse, h_j_collapse)
+            h_j = H_q[j, :, :] # hidden_size x batch
+            alpha = cosine_similarity(h_i, h_j)
             result = tf.concat([result, alpha], axis=1)
             return [j+1, result]
-        shape_invariants = [idx.get_shape(), tf.TensorShape([None, None])]
-        result = tf.while_loop(cond, body, [idx, result_init], shape_invariants = shape_invariants)
+
+        idx = tf.constant(0) # Current time step index
+        cond = lambda j, result: j < b_time_steps  
+        result_init = tf.zeros([batch_size, 1]) # batch_size x 1
+        shape_invariants = [idx.get_shape(), tf.TensorShape([batch_size, None,])]
+        result = tf.while_loop(cond, body, [idx, result_init], shape_invariants=shape_invariants)
         result = result[1][:, 1:]
         return result
-
-    # def compute_h_i_mean(alpha, H_q):
-    #     """
-    #     Args:
-    #         alpha: tensor [batch_size, B_time_steps]
-    #         H_q: tensor [batch, B_time_steps, hidden_size]
-    #     Output:
-    #         h_i_mean: tensor[1, batch_size, hidden size]
-    #     """
-    #     alpha_sum = tf.reduce_sum(alpha, axis=1) # batch_size x 1
-    #     b_time_steps = H_q.get_shape().as_list()[1]
-    #     hidden_size = H_q.get_shape().as_list()[1]
-    #     batch_size = H_q.get_shape().as_list()[2]
-
-    #     H_q_t = tf.transpose(H_q, perms=[0, 2, 1]) # batch x hidden_size x B_time_steps
-    #     alpha_expand = tf.expand_dims(alpha, axis=2) # batch x B_time_steps x 1
-    #     result = tf.matmul(H_q_t, alpha_expand) # batch x hidden x 1
-    #     return tf.transpose(result, perms=[2, 0, 1]) # 1 x batch x hidden
 
     def compute_attention(self, a, b, W):
         """
@@ -72,32 +50,20 @@ class Max_Attentive_Matching_Layer(Attention_Base_Class):
         # a_perm is now [A_time_steps, hidden_size, batch]
         a_perm = tf.transpose(a, perm=[1, 2, 0])
         a_time_steps = a_perm.get_shape()[0]
+        hidden_size = a.get_shape()[2]
         b_perm = tf.transpose(b, perm=[1,2,0])
         b_time_steps = b_perm.get_shape()[0]
 
         idx = tf.constant(0) # Current time step index
         cond = lambda i, result: i < a_time_steps
         def body(i, result):
-            h_i = tf.expand_dims(a_perm[i, :, :], axis=0) # 1 x hidden_size x batch
+            h_i = a_perm[i, :, :] #hidden_size x batch
             alpha = self.compute_alpha_embeddings(h_i, b_perm) # batch x B_time_steps
-
-            # get index for each batch of best timestep
-            max_indices = tf.argmax(alpha, axis=1) # batch x 1, time_step index
-            print ("max_indices", max_indices.get_shape())
-            # lookup = tf.one_hot(max_indices, b_time_steps) # batch x b_timesteps
-            # print lookup.get_shape()
-            h_i_mean = tf.nn.embedding_lookup(b, max_indices) # batch x hidden
-            print h_i_mean.get_shape()
-            assert False
-
-            #1 x batch_size x hidden_size
-            h_i_mean = compute_h_i_mean(alpha, b) # 1 x batch_size x hidden
-            h_i_perm = tf.transpose(h_i, perms=[0, 2, 1])
-            m_i = self.compute_score(h_i_perm, h_i_mean) # 1 x batch x perspectives
-
-
-
-            m_i = tf.expand_dims(self.compute_attentio(h_i, h_i_mean, W, a_time_steps), axis=0)
+            max_indices = tf.argmax(alpha, axis=1) # batch x 1 (time_step index)
+            dense_max = tf.expand_dims(tf.one_hot(max_indices, b_time_steps), axis=1) # batch x 1 x b_time_steps
+            h_i_mean = tf.transpose(tf.matmul(dense_max, b), perm=[1, 0, 2]) # 1 x batch x hidden
+            h_i_perm = tf.expand_dims(tf.transpose(h_i), 0) # 1 x batch size x hidden
+            m_i = tf.expand_dims(self.compute_score(h_i_perm, h_i_mean, W), axis=0) # 1 x batch x perspectives
             result = tf.concat([result, m_i], axis=0) # building time_steps x batch x persspective
             return [i+1, result]
 
@@ -106,6 +72,23 @@ class Max_Attentive_Matching_Layer(Attention_Base_Class):
         result = tf.zeros(shape=[1, batch_size, self.num_perspectives])
         result = tf.while_loop(cond, body, [idx, result], shape_invariants=shape_invariants) # A_time_steps x batch x perspectives
         return tf.transpose(result[1][1:, :, :], perm=[1, 0, 2]) # batch x A_time_steps x perspective
+
+def numpy_reference_max_aml(A, B, W, A_time_steps, B_time_steps, batch_size, num_perspectives, hidden_size):
+    A_perm = np.transpose(A, [1,2,0])
+    B_perm = np.transpose(B, [1,2,0])
+    result = np.zeros([A_time_steps, batch_size, num_perspectives])
+    for i in range(0, A_time_steps):
+        h_i = A_perm[i, :, :]
+        alpha = numpy_compute_alpha_embeddings(h_i, B_perm, B_time_steps, batch_size, hidden_size) #batch x b_time_steps
+        result_i = np.zeros([batch_size, hidden_size])
+        for j in range(0, batch_size):
+            best_time_step = np.argmax(alpha[j])
+            result_i[j] = B[j][best_time_step]
+        h_i_perm = np.expand_dims(np.transpose(h_i), 0)
+        m_i = numpy_reference_compute_score(h_i_perm, result_i, W, batch_size, hidden_size, num_perspectives) # 1 x batch x perspectives
+        result[i, :, :] = m_i
+    result = np.transpose(result, (1, 0, 2))
+    return result
 
 
 if __name__ == "__main__":
@@ -116,11 +99,16 @@ if __name__ == "__main__":
     B_time_steps = 6
 
     with tf.Session() as session:
+        batch_size = 3
+        hidden_size = 4
+        num_perspectives = 2
+        A_time_steps = 5
+        B_time_steps = 6
+
         A, B, W  = numpy_generate_A_B_w(batch_size, hidden_size, A_time_steps, B_time_steps, num_perspectives)
-        mam = Max_Attentive_Matching_Layer(num_perspectives)
-        score_fn = mam.compute_attention(tf.constant(A, dtype=tf.float32), tf.constant(B, dtype=tf.float32), tf.constant(W, dtype=tf.float32))
 
-
-
-
-
+        aml = Max_Attentive_Matching_Layer(num_perspectives)
+        score_fn = aml.compute_attention(tf.constant(A, dtype=tf.float32), tf.constant(B, dtype=tf.float32), tf.constant(W, dtype=tf.float32))
+        score = session.run(score_fn)
+        reference = numpy_reference_max_aml(A, B, W, A_time_steps, B_time_steps, batch_size, num_perspectives, hidden_size)
+        assert numpy_check_equality(score, reference, 1e-4)
